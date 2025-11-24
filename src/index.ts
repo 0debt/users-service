ab95c35764243c87cea61926b57a774fc29305d7
Joserra24 <josbanbot@alum.us.es>
2025-11-24 15:28:30 +0100

    fix:  workflow

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { authRoute } from './routes/auth'
 import { usersRoute } from './routes/users'
 import { api } from './docs/openapi'
 
+await connectToDatabase()
 
 export const app = new Hono()
 
@@ -21,19 +22,7 @@ api.route('/users', usersRoute)
 // Documentación
 app.route('/api/v1', api)
 
-if (!Bun.env.TEST) {
-  await connectToDatabase()
-}
-
-let isDbConnected = false
-
 export default {
   port: Number(Bun.env.PORT || 3000),
-  fetch: async (req: Request, env: Record<string, unknown>) => {
-    if (!isDbConnected) {
-      await connectToDatabase()
-      isDbConnected = true
-    }
-    return app.fetch(req, env)
-  }
+  fetch: app.fetch,
 }
