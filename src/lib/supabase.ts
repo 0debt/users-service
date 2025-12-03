8b736ae3bffb061ce99f274d7b3e26942fa86625
Joserra24 <josbanbot@alum.us.es>
2025-12-03 12:01:43 +0100

    fix: correction for test ci

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,27 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js"
 
-export const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY!
-);
+//Para los tests
+const isTest = process.env.TEST === "true" || process.env.CI === "true"
+
+let supabase: any
+
+if (isTest) {
+  console.warn("Supabase desactivado en modo test/CI")
+  
+  supabase = {
+    storage: {
+      from() {
+        return {
+          upload: async () => ({ data: null, error: null })
+        }
+      }
+    }
+  }
+} else {
+  supabase = createClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!
+  )
+}
+
+export { supabase }
